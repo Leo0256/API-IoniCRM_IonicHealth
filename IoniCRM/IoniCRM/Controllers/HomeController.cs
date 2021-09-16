26cfa540078cfaf7fa979b13d3729cbc1f8c4662
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2021-09-16 14:24:59 -0300

    Inicio do sistema

---
+++ IoniCRM/IoniCRM/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using IoniCRM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoniCRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private PostgreSQLConnection pgsqlcon;
+
+        public static readonly string teste = "I'm a simple text.";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            ViewData["teste"] = teste;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IoniCRM/IoniCRM/Controllers/PostgreSQLConnection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace IoniCRM.Controllers
+{
+    public class PostgreSQLConnection
+    {
+        readonly private string connString = String.Format(
+            "Server={0}; Database={1}; User id={2}; Password={3}; SslMode=Require; Trust Server Certificate=true",
+            "", /* Host */
+            "", /*  DataBase  */
+            "", /* User */
+            ""); /* Pass */
+
+
+        NpgsqlConnection conn;
+
+        public PostgreSQLConnection() => conn = new NpgsqlConnection(connString);
+
+        public DataTable ExecuteCmd(string query)
+        {
+            DataTable table = new();
+
+            try
+            {
+                using (conn = new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+                    using NpgsqlDataAdapter Adpt = new(query, conn);
+                    Adpt.Fill(table);
+                }
+            }
+            catch (Exception ex)
+            {
+                /*Mensagem de erro : ("Erro na Execução: " + ex.Message);*/
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return table;
+        }
+
+        public void Open()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Close();
+                /*Mensagem de erro : ("Erro: " + ex.Message);*/
+            }
+        }
+
+        public void Close() => conn.Close();
+    }
+}
